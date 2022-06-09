5e90cb21299cc933f4dd9a17f92ace518b608def
kirlo-s <kirlo.stnu@gmail.com>
2022-06-09 09:15:17 +0900

    update template

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from GAI import daily_calendar,weekly_calendar,information_card
 
-payload = {"owner":"owner"}
+payload = {"owner":"owner",
+    "info":{
+        "name" : "servername"
+}
+}
 
 information_card.generate(payload)
 weekly_calendar.generate(payload)
